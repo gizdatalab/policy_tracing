e0f7b97be1b633527784d54cf9dec5f9a6f3d736
Lehmann <erik.lehmann@giz.de>
2022-05-09 18:22:50 +0200

    layout

--- streamlit/main_site.py
+++ streamlit/main_site.py
@@ -7,7 +7,21 @@ def app():
         st.markdown("<h1 style='text-align: center; color: black;'>SDSN X GIZ Policy Tracing</h1>", unsafe_allow_html=True)
         st.write(' ')
         st.write(' ')
-        c1, c2, c3 = st.columns([1,7,1])
+        c1, c2, c3 = st.columns([1,5,1])
 
         c2.image("https://sustainability.yale.edu/sites/default/files/resize/images/paris_3obj_infographic%20%281%29-800x447"
                  ".png", width=1000)
+            
+    with st.expander("ℹ️ - About this app", expanded=True):
+
+        st.write(
+            """     
+    -   The *BERT Keyword Extractor* app is an easy-to-use interface built in Streamlit for the amazing [KeyBERT](https://github.com/MaartenGr/KeyBERT) library from Maarten Grootendorst!
+    -   It uses a minimal keyword extraction technique that leverages multiple NLP embeddings and relies on [Transformers] (https://huggingface.co/transformers/) 🤗 to create keywords/keyphrases that are most similar to a document.
+            """
+        )
+
+        st.markdown("")
+
+    st.markdown("")
+    st.markdown("## **📌 Paste document **")
